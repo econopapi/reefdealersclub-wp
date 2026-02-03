4309999e37ace115898086143a0df4d503020054
Daniel Limón <dlimon.munoz@gmail.com>
2026-02-03 17:10:30 -0600

    [Product Categories Block] > Se agrega bloque a functions.php

--- functions.php
+++ functions.php
@@ -135,3 +135,118 @@ add_action('wp_enqueue_scripts', 'rdc_custom_footer_assets');
 /**
  * END RDC CUSTOM FOOTER
  */
+
+
+/**
+ * RDC CUSTOM PRODUCT CATEGORIES BLOCK
+ */
+
+/**
+ * Register RDC Product Category Block
+ */
+function rdc_register_product_categories_block() {
+	// register block script
+	wp_register_script(
+		'rdc-product-categories-block',
+		get_stylesheet_directory_uri() . '/blocks/product-categories/block.js',
+		array('wp-blocks', 'wp-element', 'wp-components', 'wp-data'),
+		CHILD_THEME_RDC_CUSTOM_ASTRA_VERSION
+	);
+
+	// register block styles
+	wp_register_style(
+		'rdc-product-categories-block-editor',
+		get_stylesheet_directory_uri() . '/blocks/product-categories/editor.css',
+		array('wp-edit-blocks'),
+		CHILD_THEME_RDC_CUSTOM_ASTRA_VERSION
+	);
+
+	wp_register_style(
+		'rdc-product-categories-block',
+		get_stylesheet_directory_uri() . '/blocks/product-categories/style.css',
+		array(),
+		CHILD_THEME_RDC_CUSTOM_ASTRA_VERSION
+	);
+
+	// register the block
+	register_block_type(
+		'rdc/product-categories',
+		array(
+			'editor_script' => 'rdc-product-categories-block',
+			'editor_style' => 'rdc-product-categories-block-editor',
+			'style' => 'rdc-product-categories-block',
+			'render_callback' => 'rdc_render_product_categories_block',
+			'attributes' => array(
+				'selectedCategories' => array(
+					'type' => 'array',
+					'default' => array(),
+				),
+				'title' => array(
+					'type' => 'string',
+					'default' => 'Categorías Top',
+				),
+				'subtitle' => array(
+					'type' => 'string',
+					'default' => '',
+				),
+			),
+		)
+	);
+}
+add_action('init', 'rdc_register_product_categories_block');
+
+
+/**
+ * Render RDC Product Categories Block
+ */
+function rdc_render_product_categories_block($attributes) {
+	$selected_categories = isset($attributes['selectedCategories']) ? $attributes['selectedCategories']:array();
+	$title = isset($attributes['title']) ? $attributes['title']: 'Categorías Top';
+	$subtitle = isset($attributes['subtitle']) ? $attributes['subtitle']: '';
+
+	if (empty($selected_categories)) {
+		return '';
+	}
+
+	ob_start();
+	?>
+
+	<div class="rdc-product-categories">
+		<?php if (!empty($title)): ?>
+			<h2 class="rdc-product-categories-title"><?php echo esc_html($title); ?></h2>
+		<?php endif; ?>
+		<div class="categories-wrapper">
+			<div class="categories-container">
+				<?php foreach ($selected_categories as $cat_id):
+					$category = get_term($cat_id, 'product_cat');
+					if (!$category || is_wp_error($category)) {
+						continue;
+					}
+
+					$thumbnail_id = get_term_meta($cat_id, 'thumbnail_id', true);
+					$image_url = $thumbnail_id? wp_get_attachment_url($thumbnail_id): wc_placeholder_img_src();
+					$category_link = get_term_link($category);
+				?>
+				<div class="category-item">
+					<a href="<?php echo esc_url($category_link); ?>" class="category-link">
+						<div class="category-image-wrapper">
+							<div class="category-circle"></div>
+							<img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($category->name); ?>" class="category-image">
+						</div>
+						<h3 class="category-name"><?php echo esc_html($category->name); ?></h3>
+					</a>
+				</div>
+				<?php endforeach; ?>
+			</div>
+		</div>
+		<?php if(!empty($subtitle)): ?>
+			<h4 class="rdc-product-categories-subtitle"><?php echo esc_html($subtitle); ?></h4>
+		<?php endif; ?>
+	</div>
+	<?php
+	return ob_get_clean();
+}
+
+/**
+ * END RDC CUSTOM PRODUCT CATEGORIES BLOCK
+ */
