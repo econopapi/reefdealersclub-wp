678c4ef55abe664be5e40f722d7da26d54d25014
Daniel Limón <dlimon.munoz@gmail.com>
2026-05-05 18:04:07 -0600

    [custom-header][main-menu] Se ajusta custom header para tener comportamiento como Ocellaris.mx (categorías bien separadas/accesibles/scroll mejorado en mobile, etc...)

--- functions.php
+++ functions.php
@@ -24,6 +24,9 @@ function child_enqueue_styles() {
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
+// Load modularized theme features.
+require_once get_stylesheet_directory() . '/includes/header.php';
+
 
 /**
  * RDC CUSTOM MENUS
@@ -41,63 +44,6 @@ function rdc_register_menus() {
 } 
 add_action('init', 'rdc_register_menus');
 
-
-
-/**
- * RDC CUSTOM HEADER
- */
-
-function rdc_custom_header_assets() {
-	// custom header CSS
-	wp_enqueue_style(
-		'rdc-header-css',
-		get_stylesheet_directory_uri() . '/assets/css/custom-header.css',
-		array(),
-		CHILD_THEME_RDC_CUSTOM_ASTRA_VERSION
-	);
-
-	//custom header JS
-	wp_enqueue_script(
-		'rdc-header-js',
-		get_stylesheet_directory_uri() . '/assets/js/custom-header.js',
-		array('jquery'),
-		CHILD_THEME_RDC_CUSTOM_ASTRA_VERSION,
-		true
-	);
-
-	// Localize AJAX data for header JS
-	wp_localize_script(
-		'rdc-header-js',
-		'rdcHeader',
-		array(
-			'ajaxUrl' => admin_url('admin-ajax.php'),
-			'nonce'   => wp_create_nonce('rdc_menu_nonce'),
-		)
-	);
-}
-add_action('wp_enqueue_scripts', 'rdc_custom_header_assets');
-
-
-/**
- * Remove Astra Header
- */
-function rdc_remove_astra_header() {
-	remove_action('astra_header', 'astra_header_markup');
-}
-add_action('wp', 'rdc_remove_astra_header');
-
-
-/**
- * Add RDC Custom Header
- */
-function rdc_custom_header_markup() {
-	get_template_part('template-parts/header-custom');
-}
-add_action('astra_header', 'rdc_custom_header_markup');
-
-/**
- * END RDC CUSTOM HEADER
- */
 
 
 /**
